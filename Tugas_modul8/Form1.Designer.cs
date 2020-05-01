5bccab91e475633910e65d41e50d21d68bf6b334
Dime07 <dimasbkt7@gmail.com>
2020-05-01 18:40:30 +0700

    rental mobil

---
+++ Tugas_modul8/Form1.Designer.cs
@@ -0,0 +1,210 @@
+﻿namespace Tugas_modul8
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.harga = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.hari = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label1.Location = new System.Drawing.Point(70, 145);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(87, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Jenis Mobil";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label2.Location = new System.Drawing.Point(70, 202);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(140, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Lama Peminjaman";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label3.Location = new System.Drawing.Point(70, 270);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(117, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Tambahan Alat";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label4.Location = new System.Drawing.Point(173, 364);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Total Harga";
+            // 
+            // harga
+            // 
+            this.harga.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.harga.Location = new System.Drawing.Point(334, 361);
+            this.harga.Name = "harga";
+            this.harga.Size = new System.Drawing.Size(203, 26);
+            this.harga.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label5.Location = new System.Drawing.Point(70, 92);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(51, 20);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Nama";
+            this.label5.Click += new System.EventHandler(this.label5_Click);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.textBox5.Location = new System.Drawing.Point(231, 89);
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(203, 26);
+            this.textBox5.TabIndex = 9;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Avanza",
+            "Kijang",
+            "APV"});
+            this.comboBox1.Location = new System.Drawing.Point(231, 147);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(203, 28);
+            this.comboBox1.TabIndex = 10;
+            // 
+            // hari
+            // 
+            this.hari.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.hari.Location = new System.Drawing.Point(231, 199);
+            this.hari.Name = "hari";
+            this.hari.Size = new System.Drawing.Size(49, 26);
+            this.hari.TabIndex = 4;
+            this.hari.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.label6.Location = new System.Drawing.Point(286, 202);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 20);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Hari";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.HotTrack;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F);
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(543, 318);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(84, 30);
+            this.button1.TabIndex = 13;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "Payung",
+            "P3K"});
+            this.comboBox2.Location = new System.Drawing.Point(231, 262);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(203, 28);
+            this.comboBox2.TabIndex = 12;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(765, 431);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.harga);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.hari);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox harga;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.TextBox hari;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ Tugas_modul8/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Tugas_modul8
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label5_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            float hari, hasil, harga, alat;
+
+            if (comboBox1.SelectedItem == "Avanza")
+            {
+                if (comboBox2.SelectedItem == "Payung")
+                {
+                    harga = 200000;
+                    alat = 30000;
+                    hari = float.Parse(this.hari.Text);
+                    hasil = (hari * harga) + alat;
+                    this.harga.Text = Convert.ToString(hasil);
+                }
+                else if (comboBox2.SelectedItem == "P3K")
+                {
+                    harga = 200000;
+                    alat = 10000;
+                    hari = float.Parse(this.hari.Text);
+                    hasil = (hari * harga) + alat;
+                    this.harga.Text = Convert.ToString(hasil);
+                }
+            }
+            else if (comboBox1.SelectedItem == "Kijang")
+            {
+                if (comboBox2.SelectedItem == "Payung")
+                {
+                    harga = 150000;
+                    alat = 30000;
+                    hari = float.Parse(this.hari.Text);
+                    hasil = (hari * harga) + alat;
+                    this.harga.Text = Convert.ToString(hasil);
+                }
+                else if (comboBox2.SelectedItem == "P3K")
+                {
+                    harga = 150000;
+                    alat = 10000;
+                    hari = float.Parse(this.hari.Text);
+                    hasil = (hari * harga) + alat;
+                    this.harga.Text = Convert.ToString(hasil);
+                }
+                else if (comboBox1.SelectedItem == "APV")
+                {
+                    if (comboBox2.SelectedItem == "Payung")
+                    {
+                        harga = 250000;
+                        alat = 30000;
+                        hari = float.Parse(this.hari.Text);
+                        hasil = (hari * harga) + alat;
+                        this.harga.Text = Convert.ToString(hasil);
+                    }
+                    else if (comboBox2.SelectedItem == "P3K")
+                    {
+                        harga = 250000;
+                        alat = 10000;
+                        hari = float.Parse(this.hari.Text);
+                        hasil = (hari * harga) + alat;
+                        this.harga.Text = Convert.ToString(hasil);
+                    }
+                }
+            }
+        }
+    }
+}
